38de89b22620efa3bce00eba78525ff9062dc1ee
Majdi-10 <majdi10_joiblia10@hotmail.com>
2021-12-21 03:56:12 +0100

    testing the back end side

---
+++ Back-endEmBrico/src/controllers/test.js
@@ -0,0 +1,23 @@
+export const getTest = (req, res) => {
+    res.send('hello tunisia')
+}
+
+
+export const getTeste = (req, res) => {
+    res.send('hello tunisia')
+}
+
+export const createTest = (req, res) => {
+    res.send('hello tunisia')
+}
+
+export const DeletTest = (req, res) => {
+    res.send('hello tunisia')
+}
+export const getTestCount = (req, res) => {
+    res.send('hello tunisia')
+}
+
+export const UpdateTest = (req, res) => {
+    res.send('hello tunisia')
+}
